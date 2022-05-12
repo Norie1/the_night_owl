d7a89a1feb6ecdd9ce1b03e6ffbb757fb6c575fc
Norie1 <leguevel.jacques@gmail.com>
2022-05-12 12:21:03 +0200

    Update map forest Jacques

--- Assets/Scripts/LoadSpecificScene.cs
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -3,12 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class LoadSpecificScene : MonoBehaviour
 {
+    public string sceneName;
     private void OnTriggerEnter2D ( Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
             //Scene2 -> premier donjon Jacques
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
