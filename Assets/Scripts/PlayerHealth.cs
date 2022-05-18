96affc138345a0027a344a6e09d6219a1e7b9082
Nolwenn <nolwenn35560@gmail.com>
2022-05-18 12:49:47 +0200

    Modif script PlayerHealth
    Définition de PlayerHealth comme une instance, ajout méthode HealPlayer

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,18 +7,63 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100 ;
     public int currentHealth ;
-    public HealthBar_J healthBar;
-   void Update()
-   {
+
+    public HealthBar healthBar;
+
+    public PlayerHealth instance;
+
+    private void Awake() 
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de PlayerHealth dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    private void Start() 
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        //Appuyer sur H retire 20HP au joueur
        if(Input.GetKeyDown(KeyCode.H))
        {
            TakeDamages(20);
        }
-   }
+
+       //Appuyer sur J ajoute 20HP au joueur
+       if(Input.GetKeyDown(KeyCode.J))
+       {
+           HealPlayer(20);
+       }
+    }
    
+
+   //Inflige des dégats au joueur
     void TakeDamages(int damage)
     {
         currentHealth = currentHealth - damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    //Restaure la vie du joueur
+    void HealPlayer(int amount)
+    {
+        if(currentHealth + amount > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+
         healthBar.SetHealth(currentHealth);
     }
 
