7b2b2bcfabbfef70da09432d8e5861f6c7387f9a
antquere <105278787+antquere@users.noreply.github.com>
2022-05-23 20:36:13 +0200

    last comit of the day fight melee finish - Antoine

--- Assets/Scripts/EnemyInter/EnemyHealth.cs
+++ Assets/Scripts/EnemyInter/EnemyHealth.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
+    public int maxHealth = 250;
     public int currentHealth;
     
     public GameObject objectToDestroy;
