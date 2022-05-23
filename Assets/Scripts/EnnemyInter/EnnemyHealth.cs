cc9724315efdc01c2a71410b87e8fd7251f19e32
antquere <105278787+antquere@users.noreply.github.com>
2022-05-23 19:23:37 +0200

    debut player combat - Antoine

---
+++ Assets/Scripts/EnnemyInter/EnnemyHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+		currentHealth -= damage;
+		
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+	
+	public void Die()
+	{
+		Debug.Log("Enemy died !");
+	}
+    
+    
+}
