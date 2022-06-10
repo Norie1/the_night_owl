269c29efc397426b4a833f01662d383c669d038c
Norie1 <leguevel.jacques@gmail.com>
2022-06-10 18:45:28 +0200

    Update Jacques

---
+++ Assets/Assets_Jacques/Scripts/Enemy_FireBall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_FireBall : MonoBehaviour
+{
+    public Transform fireProjectile;
+    public GameObject projectile;
+
+    private bool check = false;
+ 
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {
+        yield return new WaitForSeconds(3f);
+        Instantiate(projectile, fireProjectile.position, fireProjectile.rotation);    
+    }
+
+
+
+}
---
+++ Assets/Assets_Jacques/Scripts/Projectil_Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectil_Enemy : MonoBehaviour
+{
+    public float projectileSpeed = 20f;
+    public GameObject impactEffect; 
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+            rb = GetComponent<Rigidbody2D>();
+            rb.velocity = transform.right * projectileSpeed;    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    { 
+        PlayerHealth player = collision.GetComponent<PlayerHealth>();
+        if(player != null )
+        {
+        player.TakeDamage(20);
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+        }
+        
+    }
+}
