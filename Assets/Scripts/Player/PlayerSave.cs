b865a0fcde5ad1d5fbca88a2ad5e63596ca343e6
Norie1 <leguevel.jacques@gmail.com>
2022-06-15 04:48:02 +0200

    Update Save Jacques

--- Assets/Scripts/Player/PlayerSave.cs
+++ Assets/Scripts/Player/PlayerSave.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerSave : MonoBehaviour
 {
     public Transform pTransform;
     public PlayerFireBall pfb;
     public PlayerHealth ph;
-    private UnityEngine.SceneManagement.Scene scene;
+
+    private Player_Info info;
+
+    private UnityEngine.SceneManagement.Scene scene;    
 
     public void SaveScene()
     {
@@ -18,12 +23,14 @@ public class PlayerSave : MonoBehaviour
 
     public void LoadScene()
     {
-        Debug.Log("Load");
-        Player_Info info = SaveSystem.LoadPlayer();
-
-        AsyncOperation asyncOp = SceneManager.LoadSceneAsync(info.scene);
-
-        asyncOp.allowSceneActivation = false;
+      if(SceneManager.GetActiveScene().name == "Menu")
+      {
+        GameObject player = GameObject.Find("Player");
+        Destroy(player);
+      }
+        scene = SceneManager.GetActiveScene();
+        info = SaveSystem.LoadPlayer();
+        Debug.Log(info.scene);
         Vector3 pos;
         pos.x = info.transform[0];
         pos.y = info.transform[1];
@@ -31,20 +38,65 @@ public class PlayerSave : MonoBehaviour
         pTransform.position = pos;
         pfb.canThrowFireBall = info.canThrow;
         ph.currentHealth = info.health;
-        while(!asyncOp.isDone)
+        //AsyncOperation asyncOp =        
+        StartCoroutine(tempo());
+        /*
+        player = GameObject.Find("Player");
+        PlayerFireBall pfbb = player.GetComponent<PlayerFireBall>();
+        pfbb = pfb;
+        PlayerHealth phh = player.GetComponent<PlayerHealth>();
+        phh = ph;
+        player.transform.position = pTransform.position;
+        
+        //targetedScene = SceneManager.GetActiveScene();
+        Debug.Log("avant");
+        SceneManager.MoveGameObjectToScene(player,SceneManager.GetSceneByName(info.scene));
+        //SceneManager.SetActiveScene(targetedScene);
+        
+        Debug.Log("après");
+      //  asyncOp.allowSceneActivation = false;
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(info.scene));
+        SceneManager.UnloadSceneAsync("Menu");
+        */
+      /*  while(!asyncOp.isDone)
         {
+            loadText.text = "Loading progress : " + (asyncOp.progress * 100 + " %");
             if(asyncOp.progress >= 0.9f)
             {
-                Debug.Log("do you want to go in Scene, press e to go");
-             if(Input.GetKeyDown(KeyCode.Space))
-             {
+                
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
                 asyncOp.allowSceneActivation = true;
             }
             }
-        }
-                
-        SceneManager.SetActiveScene(scene);
+        }*/
+        //SceneManager.SetActiveScene(scene);
+    }
 
+    IEnumerator tempo()
+    {
+
+//a revoir
+
+        SceneManager.LoadScene(info.scene,UnityEngine.SceneManagement.LoadSceneMode.Additive);
+        yield return new WaitForSeconds(4f);
+        scene = SceneManager.GetActiveScene();
+        GameObject player = GameObject.Find("Player");
+        PlayerFireBall pfbb = player.GetComponent<PlayerFireBall>();
+        pfbb = pfb;
+        PlayerHealth phh = player.GetComponent<PlayerHealth>();
+        phh = ph;
+        player.transform.position = pTransform.position;
+        
+        //targetedScene = SceneManager.GetActiveScene();
+        Debug.Log("avant");
+        SceneManager.MoveGameObjectToScene(player,SceneManager.GetSceneByName(info.scene));
+        //SceneManager.SetActiveScene(targetedScene);
+        
+        Debug.Log("après");
+      //  asyncOp.allowSceneActivation = false;
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(info.scene));
+        SceneManager.UnloadSceneAsync("Menu");
     }
 
 }
